590bf131881990b86451b8d64ae805f8293925db
panlopezv <panlopezv@outlook.com>
2016-09-17 19:29:44 -0600

    Initial commit

---
+++ TransxelaWeb/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transxelaWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the transxelaWebApp
+ */
+angular.module('transxelaWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ TransxelaWeb/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transxelaWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transxelaWebApp
+ */
+angular.module('transxelaWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
