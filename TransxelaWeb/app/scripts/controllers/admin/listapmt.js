4cd7520ebc1d8a7dc026f54dbd9f51e3b29d4635
SaulGeuss <saulgeuss@gmail.com>
2016-10-21 23:29:19 -0600

    Creacion de la vista Administracion PMT

---
+++ TransxelaWeb/app/scripts/controllers/admin/listapmt.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transxelaWebApp.controller:AdminListapmtCtrl
+ * @description
+ * # AdminListapmtCtrl
+ * Controller of the transxelaWebApp
+ */
+angular.module('transxelaWebApp')
+  .controller('AdminListapmtCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
