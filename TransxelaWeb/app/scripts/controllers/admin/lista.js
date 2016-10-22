13607b15f34177e65ef2c92bec115b28f36654b8
SaulGeuss <saulgeuss@gmail.com>
2016-10-21 22:13:02 -0600

    Modificacion de la vista Administracion Usuarios y Listos

--- TransxelaWeb/app/scripts/controllers/admin/lista.js
+++ TransxelaWeb/app/scripts/controllers/admin/lista.js
@@ -9,11 +9,8 @@
  */
 angular.module('transxelaWebApp')
   .controller('AdminListaCtrl', ['$scope', '$interval', '$log', '$timeout', 'uiGridConstants', '$http', function ($scope, $interval, $log, $timeout, uiGridConstants, $http) {
-
     var data = [];
-
     $scope.gridOptions = {
-
         showGridFooter: true,
         showColumnFooter: true,
         enableFiltering: true,
@@ -23,17 +20,14 @@ angular.module('transxelaWebApp')
                 $scope.gridApi = gridApi;
         }
     };
-
     $scope.toggleFooter = function() {
       $scope.gridOptions.showGridFooter = !$scope.gridOptions.showGridFooter;
       $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.OPTIONS);
     };
-
     $scope.toggleColumnFooter = function() {
       $scope.gridOptions.showColumnFooter = !$scope.gridOptions.showColumnFooter;
       $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.OPTIONS);
     };
-
     $http.get('http://localhost:9000/json/lista.json')
       .success(function(data) {
         // data.forEach( function(row) {
@@ -42,22 +36,5 @@ angular.module('transxelaWebApp')
 
         $scope.gridOptions.data = data;
       });
-
-
-
-    // $http.get('http://localhost:9000/json/lista.json').success(function(data){
-    //   $scope.admins = data
-    // })
-    // $scope.gridOptions = {
-    //   data: 'admins'
-    // };
-    //
-    // $http.get('http://localhost:9000/json/lista.json')
-    // .success(function(data) {
-    //   data.forEach( function(row) {
-    //     row.registered = Date.parse(row.registered);
-    //   });
-    //   $scope.gridOptions.data = data;
-    // });
 
   }]);
--- TransxelaWeb/app/scripts/controllers/admin/usuarios.js
+++ TransxelaWeb/app/scripts/controllers/admin/usuarios.js
@@ -10,9 +10,7 @@
 angular.module('transxelaWebApp')
   .controller('AdminUsuariosCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
 
-    var today = new Date();
-    var nextWeek = new Date();
-    nextWeek.setDate(nextWeek.getDate() + 7);
+
 
     $scope.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
       if( col.filters[0].term ){
@@ -29,72 +27,44 @@ angular.module('transxelaWebApp')
       },
       columnDefs: [
         // default
-        { field: 'name', headerCellClass: $scope.highlightFilteredHeader },
+        { field: 'id', headerCellClass: $scope.highlightFilteredHeader },
         // pre-populated search field
-        { field: 'gender', filter: {
+
+        { field: 'nombre', headerCellClass: $scope.highlightFilteredHeader },
+
+        { field: 'apellido', headerCellClass: $scope.highlightFilteredHeader },
+
+        { field: 'tipo', filter: {
             term: '1',
             type: uiGridConstants.filter.SELECT,
-            selectOptions: [ { value: '1', label: 'male' }, { value: '2', label: 'female' }, { value: '3', label: 'unknown'}, { value: '4', label: 'not stated' }, { value: '5', label: 'a really long value that extends things' } ]
+            selectOptions: [ { value: '1', label: 'administrador' }]
           },
           cellFilter: 'mapGender', headerCellClass: $scope.highlightFilteredHeader },
-        // no filter input
-        { field: 'company', enableFiltering: false, filter: {
-          noTerm: true,
-          condition: function(searchTerm, cellValue) {
-            return cellValue.match(/a/);
-          }
-        }},
-        // specifies one of the built-in conditions
-        // and a placeholder for the input
-        {
-          field: 'email',
-          filter: {
-            condition: uiGridConstants.filter.ENDS_WITH,
-            placeholder: 'ends with'
-          }, headerCellClass: $scope.highlightFilteredHeader
-        },
-        // custom condition function
-        {
-          field: 'phone',
-          filter: {
-            condition: function(searchTerm, cellValue) {
-              var strippedValue = (cellValue + '').replace(/[^\d]/g, '');
-              return strippedValue.indexOf(searchTerm) >= 0;
-            }
-          }, headerCellClass: $scope.highlightFilteredHeader
-        },
-        // multiple filters
-        { field: 'age', filters: [
-          {
-            condition: uiGridConstants.filter.GREATER_THAN,
-            placeholder: 'greater than'
+
+        { field: 'estado', filter: {
+            term: '1',
+            type: uiGridConstants.filter.SELECT,
+            selectOptions: [ { value: '1', label: '1' }]
           },
-          {
-            condition: uiGridConstants.filter.LESS_THAN,
-            placeholder: 'less than'
-          }
-        ], headerCellClass: $scope.highlightFilteredHeader},
-        // date filter
-        { field: 'mixedDate', cellFilter: 'date', width: '15%', filter: {
-            condition: uiGridConstants.filter.LESS_THAN,
-            placeholder: 'less than',
-            term: nextWeek
-          }, headerCellClass: $scope.highlightFilteredHeader
-        },
-        { field: 'mixedDate', displayName: "Long Date", cellFilter: 'date:"longDate"', filterCellFiltered:true, width: '15%',
-        }
+          cellFilter: 'mapGender2', headerCellClass: $scope.highlightFilteredHeader },
+
+          { field: 'telefono', headerCellClass: $scope.highlightFilteredHeader },
+
       ]
     };
 
-    $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+    $scope.gridOptions.columnDefs[0].visible = false;
+    $scope.gridOptions.columnDefs[4].visible = false;
+
+    $http.get('http://localhost:9000/json/lista.json')
       .success(function(data) {
         $scope.gridOptions.data = data;
-        $scope.gridOptions.data[0].age = -5;
+
 
         data.forEach( function addDates( row, index ){
-          row.mixedDate = new Date();
-          row.mixedDate.setDate(today.getDate() + ( index % 14 ) );
-          row.gender = row.gender==='male' ? '1' : '2';
+
+          row.tipo = row.tipo==='administrador' ? '1' : '2';
+          row.estado = row.estado==='1' ? '1' : '2';
         });
       });
 
@@ -105,16 +75,27 @@ angular.module('transxelaWebApp')
 
 
 
-
   }])
 
+  .filter('mapGender2', function() {
+  var genderHash2 = {
+    1: '1',
+    2: '0',
+  };
 
-
+  return function(input) {
+    if (!input){
+      return '';
+    } else {
+      return genderHash2[input];
+    }
+  };
+})
 
   .filter('mapGender', function() {
   var genderHash = {
-    1: 'male',
-    2: 'female'
+    1: 'administrador',
+    2: 'pmt',
   };
 
   return function(input) {
