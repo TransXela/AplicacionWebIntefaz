8e60b09335b9091de1c6fb8ab9920bb2fe180a61
SaulGeuss <saulgeuss@gmail.com>
2016-10-28 13:49:07 -0600

    Modificaion de la Lista Administracion Dueños

--- TransxelaWeb/app/scripts/controllers/admin/listadue.js
+++ TransxelaWeb/app/scripts/controllers/admin/listadue.js
@@ -8,8 +8,39 @@
  * Controller of the transxelaWebApp
  */
 angular.module('transxelaWebApp')
-  .controller('AdminListadueCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
+  .controller('AdminListadueCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants, $cookies) {
+    // $scope.apiurl = 'http://'+ $cookies.getObject('user').apiurl +':8000';
+    // $scope.apiurl = 'http://127.0.0.1:8000';
+    // $scope.gridOptions = {};
+    // var res = $http.get($scope.apiurl+'/admin/duenio/lista');
+    //   res.success(function(response, status, headers, config) {
+    //     $scope.duenios = response;
+    //
+    //
+    //         $scope.gridOptions.data = $scope.duenios;
+    //         $scope.gridOptions.columnDefs = [
+    //           {name:'Nombre',field:'nombre'},
+    //           {name:'Apellidos',field:'apellidos'},
+    //           {name:'Empresa',field:'empresa'},
+    //           {name:'Estado',field:'estado'}
+    //           ];
+    //   });
+    //   res.error(function(response, status, headers, config) {
+    //     $location.url('/404');
+    //   });
 
+    $scope.apiurl = 'http://127.0.0.1:8000';
+    var res = $http.get($scope.apiurl+'/admin/duenio/lista');
+      res.success(function(response, status, headers, config) {
+        $scope.datos = response;
+        $scope.gridOptions.data = response;
+        // data.forEach( function addDates( row, index ){
+        //   row.estado = row.estado==='1' ? '1' : '2';
+        // });
+      });
+      res.error(function(response, status, headers, config) {
+        $location.url('/404');
+      });
 
 
     $scope.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
@@ -31,48 +62,46 @@ angular.module('transxelaWebApp')
         $scope.gridApi = gridApi;
       },
       columnDefs: [
-        // default
-        { field: 'id', headerCellClass: $scope.highlightFilteredHeader },
-        // pre-populated search field
 
-        { field: 'nombre', headerCellClass: $scope.highlightFilteredHeader },
+        {name: 'Nombre', field: 'nombre', headerCellClass: $scope.highlightFilteredHeader },
 
-        { field: 'apellido', headerCellClass: $scope.highlightFilteredHeader },
+        {name: 'Apellidos', field: 'apellidos', headerCellClass: $scope.highlightFilteredHeader },
 
-        { field: 'tipo', filter: {
-            term: '1',
-            type: uiGridConstants.filter.SELECT,
-            selectOptions: [ { value: '1', label: 'dueño' }]
-          },
-          cellFilter: 'mapGender', headerCellClass: $scope.highlightFilteredHeader },
 
-        { field: 'estado', filter: {
+
+        {name: 'Estado', field: 'estado', filter: {
             term: '1',
             type: uiGridConstants.filter.SELECT,
             selectOptions: [ { value: '1', label: '1' }]
           },
           cellFilter: 'mapGender2', headerCellClass: $scope.highlightFilteredHeader },
 
-          { field: 'telefono', headerCellClass: $scope.highlightFilteredHeader },
+          {name: 'Empresa', field: 'empresa', headerCellClass: $scope.highlightFilteredHeader },
+
+          {name:'Descripcion',cellTemplate:'<div><button class="btn btn-info btn-sm" ng-click="grid.appScope.showVerModificar(row.entity.idduenio)">Ver detalles</button></div>', enableFiltering: false}
 
       ]
     };
 
-    $scope.gridOptions.columnDefs[0].visible = false;
-    $scope.gridOptions.columnDefs[3].visible = false;
-    $scope.gridOptions.columnDefs[4].visible = false;
-
-    $http.get('http://localhost:9000/json/lista.json')
-      .success(function(data) {
-        $scope.gridOptions.data = data;
+    $scope.gridOptions.columnDefs[2].visible = false;
+    // $scope.gridOptions.columnDefs[3].visible = false;
+    // $scope.gridOptions.columnDefs[4].visible = false;
 
 
-        data.forEach( function addDates( row, index ){
 
-          row.tipo = row.tipo==='dueño' ? '1' : '2';
-          row.estado = row.estado==='1' ? '1' : '2';
-        });
-      });
+
+
+    // $http.get('http://localhost:9000/json/lista.json')
+    //   .success(function(data) {
+    //     $scope.gridOptions.data = data;
+    //
+    //
+    //     data.forEach( function addDates( row, index ){
+    //
+    //       row.tipo = row.tipo==='dueño' ? '1' : '2';
+    //       row.estado = row.estado==='1' ? '1' : '2';
+    //     });
+    //   });
 
     $scope.toggleFiltering = function(){
       $scope.gridOptions.enableFiltering = !$scope.gridOptions.enableFiltering;
@@ -94,21 +123,6 @@ angular.module('transxelaWebApp')
       return '';
     } else {
       return genderHash2[input];
-    }
-  };
-})
-
-  .filter('mapGender', function() {
-  var genderHash = {
-    1: 'dueño',
-    2: 'pmt',
-  };
-
-  return function(input) {
-    if (!input){
-      return '';
-    } else {
-      return genderHash[input];
     }
   };
 });
