b405c8a37ecf8babe008011eb578ccd6aff8e716
panlopezv <panlopezv@outlook.com>
2016-11-12 17:03:45 -0600

    Deleted routes, views and controllers without using

--- TransxelaWeb/app/scripts/app.js
+++ TransxelaWeb/app/scripts/app.js
@@ -87,11 +87,6 @@ angular
     controllerAs: 'duenio/calendariopiloto'
   })
   // Operador routes
-  .when('/operador/principal', {
-    templateUrl: 'views/operador/principal.html',
-    controller: 'OperadorPrincipalCtrl',
-    controllerAs: 'operador/principal'
-  })
   .when('/operador/denunciasRuta', {
     templateUrl: 'views/operador/denunciasruta.html',
     controller: 'OperadorDenunciasrutaCtrl',
@@ -106,21 +101,6 @@ angular
     templateUrl: 'views/operador/denunciastipo.html',
     controller: 'OperadorDenunciastipoCtrl',
     controllerAs: 'operador/denunciasTipo'
-  })
-  .when('/operador/duenios', {
-    templateUrl: 'views/operador/duenios.html',
-    controller: 'OperadorDueniosCtrl',
-    controllerAs: 'operador/duenios'
-  })
-  .when('/operador/estadisticas', {
-    templateUrl: 'views/operador/estadisticas.html',
-    controller: 'OperadorEstadisticasCtrl',
-    controllerAs: 'operador/estadisticas'
-  })
-  .when('/operador/perfil', {
-    templateUrl: 'views/operador/perfil.html',
-    controller: 'OperadorPerfilCtrl',
-    controllerAs: 'operador/perfil'
   })
   .when('/operador/denuncias', {
     templateUrl: 'views/operador/denuncias.html',
