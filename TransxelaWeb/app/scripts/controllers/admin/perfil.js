58c974626e62efeae5faafeffbd84d3272961237
SaulGeuss <saulgeuss@gmail.com>
2016-11-14 05:54:37 -0600

    Cambios de controlador de

---
+++ TransxelaWeb/app/scripts/controllers/admin/perfil.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transxelaWebApp.controller:AdminPerfilCtrl
+ * @description
+ * # AdminPerfilCtrl
+ * Controller of the transxelaWebApp
+ */
+angular.module('transxelaWebApp')
+  .controller('AdminPerfilCtrl', function ($scope, apiService, $cookies) {
+    
+  });
