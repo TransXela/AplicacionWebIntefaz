949e99b67fb2b7ac6d296326aae0005a36af4c89
SaulGeuss <saulgeuss@gmail.com>
2016-11-28 12:11:18 -0600

    Modificacion Administrador

--- TransxelaWeb/app/scripts/controllers/admin/principal.js
+++ TransxelaWeb/app/scripts/controllers/admin/principal.js
@@ -9,6 +9,39 @@
  */
 angular.module('transxelaWebApp')
   .controller('AdminPrincipalCtrl', function ($scope, apiService, $location, $cookies) {
+
+    if(typeof $cookies.getObject('user') !== 'undefined' && $cookies.getObject('user')){
+      $scope.idadmin = $cookies.getObject('user').id;
+      $scope.token = $cookies.getObject('user').token;
+      apiService.obtener('/users/'+$scope.idadmin + '/?tk=' + $scope.token).
+      success(function(response, status, headers, config) {
+        $scope.admin = response;
+      }).
+      error(function(response, status, headers, config) {
+        switch(status) {
+          case 400: {
+            $location.url('/404');
+            break;
+          }
+          case 403: {
+            $location.url('/403');
+            break;
+          }
+          case 404: {
+            $location.url('/404');
+            break;
+          }
+          default: {
+            $location.url('/500');
+          }
+        }
+      });
+    }
+    else{
+      $location.url('/login');
+    }
+
+
     $scope.cerrar = function(){
       $cookies.remove('user');
       $location.url('/');
