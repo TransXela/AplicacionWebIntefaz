fd0ffd3933effb3f40effa05622258e267744787
Lester Josue Meneses Siliezar <lesterjosue1@gmail.com>
2016-11-28 13:56:56 -0600

    Corrección pagina principal

--- TransxelaWeb/app/scripts/controllers/pmt/principal.js
+++ TransxelaWeb/app/scripts/controllers/pmt/principal.js
@@ -1,34 +1,43 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name transxelaWebApp.controller:PmtPrincipalCtrl
- * @description
- * # PmtPrincipalCtr
- * Controller of the transxelaWebApp
- */
+* @ngdoc function
+* @name transxelaWebApp.controller:PmtPrincipalCtrl
+* @description
+* # PmtPrincipalCtr
+* Controller of the transxelaWebApp
+*/
 
- //mostrar numero de denuncias por rutas
+//mostrar numero de denuncias por rutas
 
- angular.module('transxelaWebApp')
-   .controller('PmtPrincipalCtrl', function ($scope, apiService, $cookies,  $location) {
-     if(typeof $cookies.getObject('user')!='undefined' && $cookies.getObject('user')){
-         $scope.token = $cookies.getObject('user').token;
-         $scope.usuario = $cookies.getObject('user').usuario;
+angular.module('transxelaWebApp')
+.controller('PmtPrincipalCtrl', function ($scope, apiService, $cookies,  $location) {
+  $scope.cerrar = function(){
+    $cookies.remove('user');
+    $location.url('/');
+  };
+  if(typeof $cookies.getObject('user')!='undefined' && $cookies.getObject('user')){
+    $scope.token = $cookies.getObject('user').token;
+    $scope.usuario = $cookies.getObject('user').usuario;
+  }
+  else{
+    $location.url('/login');
+  }
+});
+
+angular.module('transxelaWebApp')
+.controller('PmtDenRutaCtr', function ($scope, apiService, $cookies, $location) {
+  $scope.cerrar = function(){
+    $cookies.remove('user');
+    $location.url('/');
+  };
+  if(typeof $cookies.getObject('user')!='undefined' && $cookies.getObject('user')){
+    //$scope.idduenio = $cookies.getObject('user').id;
+    $scope.token = $cookies.getObject('user').token;
+    $scope.usuario = $cookies.getObject('user').usuario;
 
 
-           }
-         });
-
- angular.module('transxelaWebApp')
- .controller('PmtDenRutaCtr', function ($scope, apiService, $cookies) {
-
-   if(typeof $cookies.getObject('user')!='undefined' && $cookies.getObject('user')){
-     //$scope.idduenio = $cookies.getObject('user').id;
-     $scope.token = $cookies.getObject('user').token;
-     $scope.usuario = $cookies.getObject('user').usuario;
-
-//muestra la cantidad de buses denunciados para PMT
+    //muestra la cantidad de buses denunciados para PMT
     $scope.Lcant = [];
     apiService.obtener('/reporte/pmt/buses'+'/'+'?tk='+$scope.token)
     .success(function(data){
@@ -64,43 +73,43 @@
       }
     });
 
-//muestra la cantidad de denuncias que tiene un duenio, de acuerdo a las denuncias que reciben sus pilotos
-     apiService.obtener('/reporte/pmt/duenio'+'/'+'?tk='+$scope.token)
-     .success(function(data){
-       $scope.dueDen = data;
-       $scope.LcntD = [];
-       $scope.LdpiD = [];
-       for (var i = 0; i < $scope.dueDen.length; i++) {
-         $scope.LdpiD[i] = $scope.dueDen[i].dpi;
-         $scope.LcntD[i] = $scope.dueDen[i].cantidad;
-       }
-       var ctx = document.getElementById("dueniosGraph").getContext('2d');
-       var myChart = new Chart(ctx, {
-         type: 'line',
-         data: {
-           labels: $scope.LdpiD,
-           datasets: [{
-             backgroundColor: ["#2ecc71"],
-             label: "Número de denuncias por DUEÑO",
-             lineTension: 1,
-             pointBorderColor:"1F0D7B",
-             pointRadius: 3.5,
-             pointBackgroundColor: "#18cfdf",
-             data: $scope.LcntD
-           }]
-         }
-       });
-     })
-     .error(function(response, status, headers, config) {
-       if(status === null || status === -1){
-         $location.url('/404');
-       }
-       else if(status === 401){
-         $location.url('/403');
-       }
-     });
+    //muestra la cantidad de denuncias que tiene un duenio, de acuerdo a las denuncias que reciben sus pilotos
+    apiService.obtener('/reporte/pmt/duenio'+'/'+'?tk='+$scope.token)
+    .success(function(data){
+      $scope.dueDen = data;
+      $scope.LcntD = [];
+      $scope.LdpiD = [];
+      for (var i = 0; i < $scope.dueDen.length; i++) {
+        $scope.LdpiD[i] = $scope.dueDen[i].dpi;
+        $scope.LcntD[i] = $scope.dueDen[i].cantidad;
+      }
+      var ctx = document.getElementById("dueniosGraph").getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: $scope.LdpiD,
+          datasets: [{
+            backgroundColor: ["#2ecc71"],
+            label: "Número de denuncias por DUEÑO",
+            lineTension: 1,
+            pointBorderColor:"1F0D7B",
+            pointRadius: 3.5,
+            pointBackgroundColor: "#18cfdf",
+            data: $scope.LcntD
+          }]
+        }
+      });
+    })
+    .error(function(response, status, headers, config) {
+      if(status === null || status === -1){
+        $location.url('/404');
+      }
+      else if(status === 401){
+        $location.url('/403');
+      }
+    });
 
-// muestra cantidad de denuncias por tipo de denuncia
+    // muestra cantidad de denuncias por tipo de denuncia
     apiService.obtener('/reporte/pmt/tipoDenuncia'+'/'+'?tk='+$scope.token)
     .success(function(data){
       $scope.tipDen = data;
@@ -136,7 +145,7 @@
       }
     });
 
-//muestra las rutas denunciadas
+    //muestra las rutas denunciadas
     apiService.obtener('/reporte/pmt/rutas'+'/'+'?tk='+$scope.token)
     .success(function(data){
       $scope.rutaDen = data;
@@ -172,42 +181,44 @@
       }
     });
 
-//muestra los pilotos denunciados
-      apiService.obtener('/reporte/pmt/pilotos'+'/'+'?tk='+$scope.token)
-      .success(function(data){
-        $scope.chofDen = data;
-        $scope.LdpiC = [];
-        $scope.LcntC = [];
-        for (var i = 0; i < $scope.chofDen.length; i++) {
-          $scope.LdpiC[i] = $scope.chofDen[i].dpi;
-          $scope.LcntC[i] = $scope.chofDen[i].cantidad;
-        }
-        var ctx = document.getElementById("chofGraph").getContext('2d');
-        var myChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: $scope.LdpiC,
-            datasets: [{
-              backgroundColor: ["#2ecc71"],
-              label: "Número de denuncias por PILOTO",
-              lineTension: 1,
-              pointBorderColor:"1F0D7B",
-              pointRadius: 3.5,
-              pointBackgroundColor: "#18cfdf",
-              data: $scope.LcntC
-            }]
-          }
-        });
-      })
-      .error(function(response, status, headers, config) {
-        if(status === null || status === -1){
-          $location.url('/404');
-        }
-        else if(status === 401){
-          $location.url('/403');
+    //muestra los pilotos denunciados
+    apiService.obtener('/reporte/pmt/pilotos'+'/'+'?tk='+$scope.token)
+    .success(function(data){
+      $scope.chofDen = data;
+      $scope.LdpiC = [];
+      $scope.LcntC = [];
+      for (var i = 0; i < $scope.chofDen.length; i++) {
+        $scope.LdpiC[i] = $scope.chofDen[i].dpi;
+        $scope.LcntC[i] = $scope.chofDen[i].cantidad;
+      }
+      var ctx = document.getElementById("chofGraph").getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: $scope.LdpiC,
+          datasets: [{
+            backgroundColor: ["#2ecc71"],
+            label: "Número de denuncias por PILOTO",
+            lineTension: 1,
+            pointBorderColor:"1F0D7B",
+            pointRadius: 3.5,
+            pointBackgroundColor: "#18cfdf",
+            data: $scope.LcntC
+          }]
         }
       });
+    })
+    .error(function(response, status, headers, config) {
+      if(status === null || status === -1){
+        $location.url('/404');
+      }
+      else if(status === 401){
+        $location.url('/403');
+      }
+    });
 
 
-   }
- });
+  }else{
+    $location.url('/login');
+  }
+});
